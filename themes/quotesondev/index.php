2855f9b10ae5f310a9391e359ae533fb9d532b3b
bluebluezi <ng.johnnyzi@gmail.com>
2020-03-12 14:16:08 -0400

    feat: displays 1 random post on the frontpage

--- themes/quotesondev/index.php
+++ themes/quotesondev/index.php
@@ -1,3 +1,32 @@
 <?php get_header();?>
-    <button class = "randomQuote"> Click Me! </button>
+    
+
+    <?php
+        $args = array( 
+            'post_type' => 'post', 
+            'order' => 'ASC',
+            'orderby' => 'rand',
+            'numberposts' => 1
+            );
+        $quote_post = get_posts( $args ); 
+    ?> <!-- returns an array of posts-->
+
+
+    <?php foreach ($quote_post as $post): setup_postdata( $post );
+    ?>
+
+        <h1 class="the-quote">
+            <?php echo $post -> post_content;?>
+        </h1>
+
+        <p class="the-author">
+            <?php echo $post -> post_title;?>
+        </p>
+
+
+    <button class = "randomQuote">CLICK ME</button>
+
+    <?php endforeach;?>
+
 <?php get_footer();?>
+
--- themes/quotesondev/js/api.js
+++ themes/quotesondev/js/api.js
@@ -1,20 +1,36 @@
 (function ($) {
-    console.log(red_vars);
+    console.log(" redvars:" + red_vars);
 
     $(function () {
 
         $('.randomQuote').on('click', function (e) {
+            console.log(`this is the rest_url property: ${red_vars.rest_url}`);
+            e.preventDefault();
             $.ajax({
                 method: 'GET',
                 url:
                     red_vars.rest_url +
-                    'wp/v2/posts?filter[orderby]=rand&filter[post_per_page]=10'
+                    'wp/v2/posts?filter[orderby]=rand&filter[post_per_page]=1'
             }).done(function (data) {
-                console.log(data);
+                console.log(data); //one way to display data;
+
+                data.forEach(quote => { //quote is the key we created for each data array element
+                    console.log(quote);
+                    console.log('title:' + quote.title.rendered);
+
+                    console.log($(".the-author"));
+                    $(".the-author")[0].innerHTML = quote.title.rendered;
+                    $(".the-quote")[0].innerHTML = quote.content.rendered;
+                });
             });
 
         });
 
+
+        //$('.submitQuote).on('click), function(e){
+
+    }
+
     });
 
-})(jQuery);
+}) (jQuery);
