114dc5ce71998bef72276257e76da65f8b48dfdc
bluebluezi <ng.johnnyzi@gmail.com>
2020-03-12 16:46:21 -0400

    style: added elements and appropriate wrappers/containers for the footer

--- themes/quotesondev/footer.php
+++ themes/quotesondev/footer.php
@@ -1,4 +1,14 @@
 <?php wp_footer();?>
-
+<footer>
+    <nav class="main-menu">
+        <?php wp_nav_menu(array(
+            'theme_location' => 'primary'
+        ));?>
+    </nav>
+    <div class="footer-text-container">
+        <span>Brought to you by</span>
+        <a href="https://redacademy.com/">RED Academy</a>
+    </div>
+</footer>
 </body>
 </html>
