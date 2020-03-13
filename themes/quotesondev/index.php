a9b1736843ca3a83c07f1196f145c2ca56e4e5ca
bluebluezi <ng.johnnyzi@gmail.com>
2020-03-13 13:48:31 -0400

    style: styled front page content area, changed class for button

--- themes/quotesondev/index.php
+++ themes/quotesondev/index.php
@@ -1,6 +1,5 @@
 <?php get_header();?>
     
-
     <?php
         $args = array( 
             'post_type' => 'post', 
@@ -11,22 +10,23 @@
         $quote_post = get_posts( $args ); 
     ?> <!-- returns an array of posts-->
 
+     <section class="home-content">
+        <div class="home-content-container">       
+   
+            <?php foreach ($quote_post as $post): setup_postdata( $post );?>
 
-    <?php foreach ($quote_post as $post): setup_postdata( $post );
-    ?>
+            <h1 class="the-quote">
+                <?php echo $post -> post_content;?>
+            </h1>
+            <p class="the-author">
+                &mdash; <?php echo $post -> post_title;?>
+            </p>
+            <button class = "random-quote">Show Me Another!</button>
 
-        <h1 class="the-quote">
-            <?php echo $post -> post_content;?>
-        </h1>
+            <?php endforeach;?>
 
-        <p class="the-author">
-            <?php echo $post -> post_title;?>
-        </p>
-
-
-    <button class = "randomQuote">CLICK ME</button>
-
-    <?php endforeach;?>
+        </div>
+    </section>
 
 <?php get_footer();?>
 
