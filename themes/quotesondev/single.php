f8d71845b7c6de4e83773d9b77998a086ebe083c
bluebluezi <ng.johnnyzi@gmail.com>
2020-03-17 03:51:05 -0400

    style: updated the template to look (visually)  the same as home.php

--- themes/quotesondev/single.php
+++ themes/quotesondev/single.php
@@ -1,21 +1,50 @@
 <?php get_header(); ?>
 
+<section class="home-content">
+    <div class="home-content-container">
+
 <?php if( have_posts() ) :
 //The WordPress Loop: loads post content 
     while( have_posts() ) :
         the_post(); ?>
     
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+    <h2 class="the-quote">
+        <?php echo $post -> post_content;?>
+    </h2>
+    <p class="the-author">
+                &mdash; <?php echo $post -> post_title;?>
+                <?php 
+                
+                    $fieldUrl = get_post_meta(get_the_ID(),'_qod_quote_source_url',true);
+                    $fieldSource = get_post_meta(get_the_ID(),'_qod_quote_source',true);
+
+                    if($fieldUrl && $fieldSource){ 
+                        echo ", ";
+                ;?>
+
+                        <a href = "<?php echo $fieldUrl;?>">
+                            <?php echo  $fieldSource;?>
+                        </a>
+                <?php
+                    } elseif($fieldSource){
+                        echo ", " . $fieldSource;
+                    }
+                ;?>
+             
+            </p>
+            <button class = "random-quote">Show Me Another!</button>
+
     
     <!-- Loop ends -->
     <?php endwhile;?>
 
-    <?php the_posts_navigation();?>
 
 <?php else : ?>
         <p>No posts found</p>
 <?php endif;?>
+
+    </div>
+</section>
 
     
 <?php get_footer();?>
